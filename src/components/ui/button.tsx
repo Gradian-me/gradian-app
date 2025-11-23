6958ddd00c8dc040d645fb1320a702233b49f71e
Mahyari88 <abidi.mahyar@gmail.com>
2025-11-23 13:58:06 +0330

    UPDATE: Adjust timestamps in all-data.json for improved data accuracy. MODIFY: Refactor button styles in button.tsx and Button.tsx components to enhance visual consistency and dark mode support.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-red-500 text-white shadow-sm hover:bg-red-600 hover:shadow-md dark:bg-red-600 dark:text-white dark:hover:bg-red-700",
         outline:
-          "border border-none bg-white dark:bg-gray-700 dark:hover:bg-violet-700 dark:border-violet-700 hover:bg-violet-50 hover:border hover:border-violet-300 dark:text-violet-300 dark:hover:border-violet-300 text-violet-700 shadow-sm hover:shadow-md",
+          "border border-none bg-gray-50 dark:bg-gray-700 dark:hover:bg-violet-700 dark:border-violet-700 hover:bg-violet-50 hover:border hover:border-violet-300 dark:text-violet-300 dark:hover:border-violet-300 text-violet-700 shadow-sm hover:shadow-md",
         secondary:
           "bg-gray-100 text-gray-900 hover:bg-gray-200 shadow-sm hover:shadow-md dark:bg-gray-700 dark:text-gray-100 dark:hover:bg-gray-600",
         ghost: "text-violet-600 hover:bg-violet-50 hover:text-violet-700 dark:text-violet-300 dark:hover:bg-gray-800 dark:hover:text-violet-300",
